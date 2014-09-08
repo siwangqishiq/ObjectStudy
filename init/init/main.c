c12a0fd7744f0aae1b11a826668cc1c0647923ab
panyi <525647740@qq.com>
2014-09-08 11:31:25 +0800

    insert

---
+++ init/init/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  init
+//
+//  Created by  潘易  on 14-9-8.
+//  Copyright (c) 2014年 xinlan. All rights reserved.
+//
+// test
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
